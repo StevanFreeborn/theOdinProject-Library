d76803789952aa2959e48a9160e63d6920b44636
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-05-11 21:21:47 -0500

    tests: finish writing tests for bookService

--- tests/bookService.spec.js
+++ tests/bookService.spec.js
@@ -1,16 +1,20 @@
-import { jest } from '@jest/globals';
-import * as bookService from '../js/bookService.js';
+import {
+  addBook,
+  deleteBookById,
+  getBooks,
+  updateBook,
+  updateBooks,
+} from '../js/bookService.js';
 import { BOOKS_KEY } from '../js/constants.js';
 
 describe('bookService', () => {
-  describe('getBooks', () => {
-    beforeEach(() => {
-      localStorage.clear();
-      jest.restoreAllMocks();
-    });
+  beforeEach(() => {
+    localStorage.clear();
+  });
 
+  describe('getBooks', () => {
     test('should return an empty array if no books are in local storage', () => {
-      const books = bookService.getBooks();
+      const books = getBooks();
       expect(books).toEqual([]);
     });
 
@@ -26,13 +30,13 @@ describe('bookService', () => {
         JSON.stringify(storedBooks)
       );
 
-      const books = bookService.getBooks();
+      const books = getBooks();
 
       expect(books).toEqual(storedBooks);
     });
 
     test('should store an empty array in local storage and return it if no books are in local storage', () => {
-      const books = bookService.getBooks();
+      const books = getBooks();
       const storedBooks = JSON.parse(
         localStorage.getItem(BOOKS_KEY)
       );
@@ -42,11 +46,114 @@ describe('bookService', () => {
     });
   });
 
-  describe('updateBooks', () => {
-    beforeEach(() => {
-      localStorage.clear();
+  describe('addBook', () => {
+    const book1 = { title: 'Book 1', author: 'Author 1' };
+    const book2 = { title: 'Book 2', author: 'Author 2' };
+    const books = [book1];
+
+    it('should return a new array with the added book', () => {
+      const newBooks = addBook({ book: book2, books });
+      expect(newBooks).toEqual([...books, book2]);
     });
 
+    it('should not modify the original array of books', () => {
+      addBook({ book: book2, books });
+      expect(books).toEqual([book1]);
+    });
+  });
+
+  describe('updateBook', () => {
+    let books;
+
+    beforeEach(() => {
+      books = [
+        { id: 1, title: 'Book 1' },
+        { id: 2, title: 'Book 2' },
+        { id: 3, title: 'Book 3' },
+      ];
+    });
+
+    it('should update the book with the given id', () => {
+      const updatedBook = {
+        id: 2,
+        title: 'Updated Book 2',
+      };
+
+      const result = updateBook({ updatedBook, books });
+
+      expect(result).toEqual([
+        { id: 1, title: 'Book 1' },
+        { id: 2, title: 'Updated Book 2' },
+        { id: 3, title: 'Book 3' },
+      ]);
+    });
+
+    it('should not update any book if the given id is not found', () => {
+      const updatedBook = { id: 4, title: 'New Book' };
+
+      const result = updateBook({ updatedBook, books });
+
+      expect(result).toEqual([
+        { id: 1, title: 'Book 1' },
+        { id: 2, title: 'Book 2' },
+        { id: 3, title: 'Book 3' },
+      ]);
+    });
+
+    it('should return a new array of books', () => {
+      const updatedBook = {
+        id: 2,
+        title: 'Updated Book 2',
+      };
+
+      const result = updateBook({ updatedBook, books });
+
+      expect(result).not.toBe(books);
+    });
+  });
+
+  describe('deleteBookById', () => {
+    let books;
+    let bookId;
+    beforeEach(() => {
+      books = [
+        { id: '1', title: 'Book 1' },
+        { id: '2', title: 'Book 2' },
+        { id: '3', title: 'Book 3' },
+      ];
+      bookId = '2';
+    });
+
+    it('should remove the book with the given id from the array of books', () => {
+      const result = deleteBookById({ bookId, books });
+      expect(result).toHaveLength(2);
+      expect(
+        result.find(book => book.id === bookId)
+      ).toBeUndefined();
+    });
+
+    it('should return a new array of books without the deleted book', () => {
+      const result = deleteBookById({ bookId, books });
+      expect(result).not.toBe(books);
+    });
+
+    it('should not modify the original array of books', () => {
+      const result = deleteBookById({ bookId, books });
+      expect(books).toHaveLength(3);
+      expect(books).toEqual([
+        { id: '1', title: 'Book 1' },
+        { id: '2', title: 'Book 2' },
+        { id: '3', title: 'Book 3' },
+      ]);
+    });
+
+    it('should return the original array of books if the book with the given id does not exist', () => {
+      const result = deleteBookById({ bookId: '4', books });
+      expect(result).toEqual(books);
+    });
+  });
+
+  describe('updateBooks', () => {
     test('should set the books array to local storage', () => {
       const books = [
         { title: 'Book 1', author: 'Author 1' },
@@ -54,7 +161,7 @@ describe('bookService', () => {
         { title: 'Book 3', author: 'Author 3' },
       ];
 
-      bookService.updateBooks({ books });
+      updateBooks({ books });
 
       const storedBooks = JSON.parse(
         localStorage.getItem(BOOKS_KEY)
@@ -81,7 +188,7 @@ describe('bookService', () => {
         },
       ];
 
-      bookService.updateBooks({ books: newBooks });
+      updateBooks({ books: newBooks });
 
       const storedBooks = JSON.parse(
         localStorage.getItem(BOOKS_KEY)
